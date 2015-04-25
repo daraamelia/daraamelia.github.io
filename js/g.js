5a63a38399045483f852f0f8b740c70f1e48bf34
daraamelia <infoterbaru@cw.jitu.pw>
2015-04-25 08:51:31 +0700

    Create g.js

---
+++ js/g.js
@@ -0,0 +1,1 @@
+(function(){var po=document.createElement(&#39;script&#39;);po.type=&#39;text/javascript&#39;po.async=true;po.src=&#39;https:var s=document.getElementsByTagName(&#39;script&#39;)[0];s.parentNode.insertBefore(po,s)})();
