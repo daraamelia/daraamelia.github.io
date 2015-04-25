0ca75adb190b5b0fcde4521df338b923e492d001
daraamelia <infoterbaru@cw.jitu.pw>
2015-04-25 08:53:57 +0700

    Create t.js

---
+++ js/t.js
@@ -0,0 +1,1 @@
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=&quot;//platform.twitter.com/widgets.js&quot;;fjs.parentNode.insertBefore(js,fjs);}}(document,&quot;script&quot;,&quot;twitter-wjs&quot;);
