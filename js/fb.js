8599572f731bf0a715095f2731ad9f8b8396221b
daraamelia <infoterbaru@cw.jitu.pw>
2015-04-25 08:49:17 +0700

    Create fb.js

---
+++ js/fb.js
@@ -0,0 +1,1 @@
+(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id)){return}js=d.createElement(s);js.id=id;js.src=&quot;http:fjs.parentNode.insertBefore(js,fjs)}(document,&#39;script&#39;,&#39;facebook-jssdk&#39;));
